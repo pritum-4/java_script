6676f9c740013c6141be9c747a25f2f654766ffa
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-04-04 09:53:04 +0000

    new datatype file added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";//treat all JS code newer versions
+console.log("Data types")
+//alert("Hello")==>will not work in node
+console.log(3+3);
+//ecma script is the standard for the javascript;
+let name= "Prayas"
+let age = 18
+let isLoggedIn = false
+
+//number =>2 to power 53
+//bigint
+//string = ""
+//boolean = true / false
+//null is a standalone value
+//undqefined=>VALUE IS NOT STILL DEFINED
+//symbol=>UNIQUE
+//object=>
+console.log(typeof null)//object
+console.log(typeof undeqfined)//undefined
+console.log(typeof -53)//number
+console.log("=========================")
+
+
+
+
+
+
+
+
+
+
+
+

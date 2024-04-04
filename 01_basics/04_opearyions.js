55532d0dc0ddc346ade6858fa330bbe245050436
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-04-04 10:43:44 +0000

    file added

---
+++ 01_basics/04_opearyions.js
@@ -0,0 +1,14 @@
+let str1 = "Prayas"
+let str2 = "Neon"
+let str3 = str1+str2;
+console.log(str3);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+"2");
+console.log(+true);
+console.log(+"");
+//not good to use
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1,num2,num3)
+

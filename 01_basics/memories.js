49195e67829112c4682978bac0e8a2dfac3f5c40
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-04-04 11:30:20 +0000

    memory file added

---
+++ 01_basics/memories.js
@@ -0,0 +1,18 @@
+STACK AND HEAP MEMORY
+
+-primitive datatype uses STACK
+(call by value)
+ -String
+ -Boolean
+ -Number
+ -null
+ -undefined
+ -BigInt
+ -Symbol
+-reference/nonprimitive datatype uses HEAP
+(call by reference)
+ -Array
+ -function
+ -Object
+
+JAVASCRIPT is a dynamically typed language that means you donot need to specify the datatype when you declare it

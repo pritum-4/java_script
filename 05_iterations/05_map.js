0c35eb5156bf6491b04657a714db61a897137888
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-04-08 15:18:31 +0000

    new file added

--- 05_iterations/05_map.js
+++ 05_iterations/05_map.js
@@ -1,7 +1,13 @@
 const arr = [1,2,3,4,5,6,7,8,9,10]
 const arr1 = arr.map((num)=>num+10)
-console.log(arr1);
+console.log(arr1);//[
+    11, 12, 13, 14, 15,
+    16, 17, 18, 19, 20
+  //]
 
-const arr2 = arr.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>=40);
+const arr2 = arr.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>=40);//chaining 
 
-console.log(arr2);
+console.log(arr2);//[
+    41, 51,  61, 71,
+    81, 91, 101
+  //]

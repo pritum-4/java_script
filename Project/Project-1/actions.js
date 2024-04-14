f91af44e9f1b591097de0c7d61bc37add88e66aa
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-04-14 04:42:25 +0000

    file added

--- Project/Project-1/actions.js
+++ Project/Project-1/actions.js
@@ -1,7 +1,20 @@
 const buttons = document.querySelectorAll('.button');
-buttons.forEach((button)=>{
-    button.addEventListener('click',(e)=>{
-        console.log(e);
-        console.log(e.target);
-    })
-})
+const body = document.querySelector('body');
+buttons.forEach((button) => {
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target.id);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});

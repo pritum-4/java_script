00cc9feabcf8335115193eb6871ab924698654da
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-04-15 16:05:28 +0000

    changes made

--- javascipt_practice/p2.js
+++ javascipt_practice/p2.js
@@ -1,8 +1,9 @@
 let n = 12345;
-let y=0;
-while(n>0)
-{
-    y=y*10+(n%10);
-    n=parseInt(n/10);
-}
+// let y=0;
+// while(n>0)
+// {
+//     y=y*10+(n%10);
+//     n=parseInt(n/10);
+// }
+let y = parseInt(n.toString().split('').reverse().join(''))
 console.log(y);
